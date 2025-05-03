dfc0ee468982f93203e2d10e10012f938cb2090f
Randomx2006 <ransome458@gmail.com>
2025-05-03 11:55:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,33 @@
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 import os
-
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
 import pyrogram
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+class Config:
+    DOWNLOAD_LOCATION = "./downloads"
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7591552604:AAHrwJwmVGwxbNTDphs-ku9wTisamrUuk0U")
+    APP_ID = int(os.environ.get("APP_ID","25132804"))
+    API_HASH = os.environ.get("API_HASH", "843d95d64eba173d7ef49ed4bb1440a8")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "7686184938").split(","))
 
-if __name__ == "__main__" :
-    if not os.path.isdir(Config.D:\):
-        os.makedirs(Config.D:\)
-    plugins = dict(
-        root="plugins"
-    )
+if __name__ == "__main__":
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    
+    plugins = dict(root="plugins")
+    
     app = pyrogram.Client(
         "RenameBot",
-        bot_token=Config.7591552604:AAHrwJwmVGwxbNTDphs-ku9wTisamrUuk0U,
-        api_id=Config.25132804,
-        api_hash=Config.843d95d64eba173d7ef49ed4bb1440a8,
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(861055237)
+    
     app.run()
